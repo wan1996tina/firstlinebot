8cfeaf795a0a7f808a3799e14e2911293b758420
wan1996tina <45960498+wan1996tina@users.noreply.github.com>
2020-05-17 23:15:55 +0800

    串網頁資料

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import linebot from 'linebot'
 import dotenv from 'dotenv'
+import rp from 'request-promise'
+import cheerio from 'cheerio'
 
 dotenv.config()
 
@@ -10,9 +12,45 @@ const bot = linebot({
 
 })
 
+const search = async (keyword) => {
+  let msg = ''
+  let kw = encodeURI(keyword)
+  console.log(kw)
+  try {
+    const result = rp({
+      uri: `https://cookpad.com/tw/%E6%90%9C%E5%B0%8B/${kw}`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    // console.log(result)
+
+    const $ = cheerio.load(result)
+    msg = 'success'
+
+
+  }
+  catch (error) {
+    msg = error.message
+  }
+
+  // console.log(result)
+  return msg
+}
+
 bot.on('message', event => {
+  let msg
+  if (event.message.text.includes('搜尋')) {
+    msg = await search(event.message.text.slice(3))
+    //msg = encodeURI(event.message.text.slice(3))
+  }/*
+  else if (event.message.text.includes('食譜')) 
+  { 
+    msg = recipe(event.message.text.slice(3))
+  }*/
   console.log(event)
   event.reply(event.message.text)
+  event.reply(msg)
 })
 
 bot.listen('/', process.env.Port)
